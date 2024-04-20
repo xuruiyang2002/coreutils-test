f4c0946c2f3609fe24f3d427b075447b06a4b80c
xuruiyang2002 <xury5520@outlook.com>
2024-04-20 14:29:13 +0800

    Refine the rebuild script

--- coreutils-9.4-bc/rebuild.sh
+++ coreutils-9.4-bc/rebuild.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-rm nohup*
-rm -rf result_all
-cd ../../recolossus/krpk
-cargo build
-cd ../../recolossus/build
-ninja klee
-cd /home/user/coreutils-test/coreutils-9.4-bc
 
+cd /home/user/recolossus/krpk
+cargo build --release
 
-# /home/user/recolossus/build/bin/klee --recolossus --max-solver-time=30 --search=bfs --external-calls=all --libc=uclibc --posix-runtime -max-time=600 
+cd /home/user/recolossus/build
+ninja klee -j8
